bf17041b17b4932627d0fee95805fc3529fa1f0a
n-zaitsev <zajatsnik@gmail.com>
2023-12-10 16:35:15 +0100

    Fix image details cell

--- pixabay-images/Modules/ImagesDetails/View/Cells/ImageDetailsCollectionViewCell.swift
+++ pixabay-images/Modules/ImagesDetails/View/Cells/ImageDetailsCollectionViewCell.swift
@@ -13,28 +13,29 @@ final class ImageDetailsCollectionViewCell: UICollectionViewCell {
         super.init(frame: .zero)
         setupConstraints()
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
     func setup(image: ImageDetails) {
         imageView.kf.setImage(with: image.webformatURL)
     }
-
+    
     private lazy var imageView: UIImageView = {
         let imageView = UIImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.clipsToBounds = true
-        imageView.contentMode = .scaleAspectFit
+        imageView.contentMode = .scaleToFill
         return imageView
     }()
 
     private func setupConstraints() {
         contentView.addSubview(imageView)
         NSLayoutConstraint.activate([
-            imageView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
-            imageView.topAnchor.constraint(equalTo: contentView.topAnchor)
+            imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor)
         ])
     }
 }
